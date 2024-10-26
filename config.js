f0d40c73c1fb408e93f5e7a638dcd2dcf50f8a94
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-10-26 13:50:44 +0700

    init card

---
+++ config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        my_teal: '#008080',
+        pale_orange: '#ffd9a6',
+        light_orange: '#fbb03b',
+        orange: '#f7931e',
+      },
+      fontFamily: {
+        'sans': ['Josefin Sans', 'sans-serif'],
+        'title': ['Playfair Display SC', 'serif'],
+      }
+    }
+  }
+}
